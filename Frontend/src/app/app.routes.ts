a01a01691f265eb6a17b0c70e619f8de23f70fb7
Hungho09 <hohung262@gmail.com>
2026-03-14 23:44:24 +0700

    feature(BVX-34): hoàn thiện trang quản lý vận chuyển với các chức năng CRUD và giao diện premium

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -4,8 +4,12 @@ import { Dashboard } from './pages/dashboard/dashboard';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
 import { Homepage } from './pages/homepage/homepage';
+// Thử bỏ chữ '.component' nếu file trong thư mục của bạn chỉ là resource-management.ts
+import { ResourceManagement } from './pages/resource-management/resource-management';
+
 
 export const routes: Routes = [
+  { path: 'resource', component: ResourceManagement },
   { path: '', redirectTo: 'homepage', pathMatch: 'full' },
   { path: 'homepage', component: Homepage },
   { path: 'dashboard', component: Dashboard },
